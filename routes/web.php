125c28073c49c2b6682d7363ad09ea9b90bc0f56
raiz <raiz@uitm.edu.my>
2026-08-12 15:19:30 +0800

    Add dashboard functionality with booking statistics and charts

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 
 Route::get('/', function () {
-    return redirect()->route('bookings.index');
+    return redirect()->route('dashboard');
 });
 
+Route::get('dashboard', [BookingController::class, 'dashboard'])->name('dashboard');
 Route::resource('bookings', BookingController::class)->only(['index', 'create', 'store', 'destroy']);
 Route::get('bookings/calendar', [BookingController::class, 'calendar'])->name('bookings.calendar');
